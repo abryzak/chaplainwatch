319d26b3c252eda7746a18302f1f3c77da29f7aa
LB (Ben) Johnston <lb@lifechurchcentral.com>
2014-01-09 18:10:18 +1000

    Early Draft Work

--- chaplain-watch.js
+++ chaplain-watch.js
@@ -1,3 +1,11 @@
+/*
+1. See if there is a geolocate IP meteorite thing
+2. loading progress
+3. MUST have better radio/checkboxes
+4. REMEMBER this is just a static version!
+4. Install fontawesome (not sure on the mrt package)
+*/
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to chaplain-watch.";
